7a362acffffdb63f7c045470f41eef74fdc9eafb
ArnoldOlanda <olanda188@gmail.com>
2023-12-27 10:05:19 -0500

    Change location firebase config file

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import Server from "./server";
 
 import * as admin from "firebase-admin";
 
-import serviceAccount from "../todo-app-rn-a9d9b-firebase-adminsdk-gsj9u-042f956f54.json";
+import serviceAccount from "./todo-app-rn-a9d9b-firebase-adminsdk-gsj9u-042f956f54.json";
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
